cae4ac4d3a230c1fe82b96e3635a62655e0605d2
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2023-10-24 00:25:36 +0800

    Added a small hack to exclude the usage of PSN00BSDK
    Before this, I think most imports were coming from PSN00BSDK without noticing.

--- common.mk
+++ common.mk
@@ -1,7 +1,7 @@
 PSN00BSDK  ?= /opt/psn00bsdk/
 PREFIX		= mipsel-unknown-elf-
-INCLUDE	   := # -I$(realpath $(PSN00BSDK)/libpsn00b/include)
-LIBDIRS	   := -L$(realpath $(PSN00BSDK)/libpsn00b)
+#INCLUDE	   := # -I$(realpath $(PSN00BSDK)/libpsn00b/include)
+#LIBDIRS	   :=  -L$(realpath $(PSN00BSDK)/libpsn00b)
 GCC_VERSION	= 7.4.0
 GCC_BASE	= /usr/local/mipsel-unknown-elf
 
@@ -45,14 +45,25 @@ BASEDIR = $(dir $(lastword $(MAKEFILE_LIST)))
 
 INCLUDE	    += -I$(realpath $(BASEDIR)/INCLUDE)
 LIBDIRS		+= -L$(realpath $(BASEDIR)/LIB)
-# default libs
-LIBS        += -lc -lc2 -lapi
+# default libs (check later for explaination on -lpsnoobc)
+LIBS        += -lpsnoobc -lc -lc2 -lapi
 
 TARGET_EXE  := $(TARGET:.elf=.exe)
 
+# Here, we are going to copy the libc.a from the PSN00BSDK to our LIB directory
+# we don't want to rely on psn00bsdk libraries, since this defeats the purpose of using PSYQ libraries
+# we are only using the libc.a from the PSN00BSDK
+# This is only used because we need export for `_start` symbol since it does setup before calling `main`
+# and PSYQ doesn't do that.
+# TODO: would be better to have our own small library that has this `_start` symbol functionality
+LIBC_START := $(realpath $(BASEDIR)/LIB)/libpsnoobc.a
+
+# we copy the file needed, and then remove it after linking
 build/$(TARGET): $(OFILES)
+	@cp $(realpath $(PSN00BSDK)/libpsn00b)/libc.a $(LIBC_START)
 	@echo "LD     $(TARGET)"
 	@$(LD) $(LDFLAGS) $(LIBDIRS) $(OFILES) $(LIBS) -o build/$(TARGET)
+	@rm $(LIBC_START)
 
 $(TARGET_EXE): build/$(TARGET)
 	@echo "ELF2X  $(TARGET:.elf=.exe)"
